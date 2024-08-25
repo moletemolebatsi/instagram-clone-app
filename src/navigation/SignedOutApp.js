57eded1630ab397c3723736f728ecbb25a31583b
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-25 03:15:33 +0200

    Update SignedOutApp.js

--- src/navigation/SignedOutApp.js
+++ src/navigation/SignedOutApp.js
@@ -1,25 +1,25 @@
-import Login from "../screens/Login";
-import Forgot from "../screens/Forgot";
+import LoginApp from "../screens/LoginApp";
+import ForgotApp from "../screens/ForgotApp";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Signup from "../screens/Signup";
+import SignupApp from "../screens/SignupApp";
 
 const Stack = createNativeStackNavigator();
 
 const SignedOutApp = () => (
   <NavigationContainer>
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Login" component={LoginApp} />
       <Stack.Screen
         name="Signup"
-        component={Signup}
+        component={SignupApp}
         options={{
           animation: "slide_from_right",
         }}
       />
       <Stack.Screen
         name="Forgot"
-        component={Forgot}
+        component={ForgotApp}
         options={{
           animation: "slide_from_right",
         }}
