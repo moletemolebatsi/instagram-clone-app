3d81f51e63d7de35d0299d9d82906c0dca433fa3
moletemolebatsi <43677651+moletemolebatsi@users.noreply.github.com>
2024-08-25 02:30:23 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
-import AuthNavigation from './src/navigation/AuthNavigation';
+import AppNav from './src/navigation/AppNav';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
